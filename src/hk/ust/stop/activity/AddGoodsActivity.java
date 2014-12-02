07694bb71b40864eb41238ee2a58b23a0313a6f9
JoyXieIsCool <joy.junrong.xie@gmail.com>
2014-12-02 22:46:18 +0800

    add goods activity

--- src/hk/ust/stop/activity/AddGoodsActivity.java
+++ src/hk/ust/stop/activity/AddGoodsActivity.java
@@ -1,16 +1,68 @@
 package hk.ust.stop.activity;
 
+import hk.ust.stop.dao.PictureDaoImpl;
+
+import java.io.File;
+import java.util.Calendar;
+import java.util.Locale;
+
 import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.text.TextUtils;
+import android.text.format.DateFormat;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
 
-public class AddGoodsActivity extends Activity {
+public class AddGoodsActivity extends Activity implements OnClickListener{
 
+	private Button captureButton;
+	private Button deleteButton;
+	private Button resetButton;
+	private Button saveButton;
+	private ImageView imageView;
+	private EditText productName;
+	private EditText productDescription;
+	private EditText productPrice;
+	
+	private String tempPicPath;
+	private Bitmap currentBitmap;
+	private String currentFileName;
+	private PictureDaoImpl dao;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_addgoods);
+		
+		dao = PictureDaoImpl.getInstance();
+		
+		captureButton = (Button) findViewById(R.id.captureButton);
+		deleteButton = (Button) findViewById(R.id.deleteButton);
+		resetButton = (Button) findViewById(R.id.resetButton);
+		saveButton = (Button) findViewById(R.id.saveButton);
+		imageView = (ImageView) findViewById(R.id.imageView);
+		productName = (EditText) findViewById(R.id.productName);
+		productDescription = (EditText) findViewById(R.id.productDescription);
+		productPrice = (EditText) findViewById(R.id.productPrice);
+		
+		tempPicPath = dao.getDirectory() + "temp.jpg";
+
+		captureButton.setOnClickListener(this);
+		deleteButton.setOnClickListener(this);
+		resetButton.setOnClickListener(this);
+		saveButton.setOnClickListener(this);
 	}
 
 	@Override
@@ -31,4 +83,120 @@ public class AddGoodsActivity extends Activity {
 		}
 		return super.onOptionsItemSelected(item);
 	}
+	
+
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		super.onActivityResult(requestCode, resultCode, data);
+		if (Activity.RESULT_FIRST_USER == requestCode) {
+			String sdStatus = Environment.getExternalStorageState();
+			if (!sdStatus.equals(Environment.MEDIA_MOUNTED)) { // ¼ì²âsdÊÇ·ñ¿ÉÓÃ
+				return;
+			}
+
+			File file = new File(tempPicPath);
+			if (file.exists()) {
+				Toast.makeText(this, file.getName(), Toast.LENGTH_LONG).show();
+				BitmapFactory.Options bmpFactoryOptions = new BitmapFactory.Options();
+				bmpFactoryOptions.inJustDecodeBounds = true;
+
+				int dw = imageView.getWidth();
+				int dh = imageView.getHeight();
+				currentBitmap = BitmapFactory.decodeFile(tempPicPath,
+						bmpFactoryOptions);
+				int heightRatio = (int) Math.ceil(bmpFactoryOptions.outHeight
+						/ (float) dh);
+				int widthRatio = (int) Math.ceil(bmpFactoryOptions.outWidth
+						/ (float) dw);
+
+				if (heightRatio > 1 && widthRatio > 1) {
+
+					if (heightRatio > widthRatio) {
+
+						bmpFactoryOptions.inSampleSize = heightRatio;
+					} else {
+						bmpFactoryOptions.inSampleSize = widthRatio;
+					}
+
+				}
+				
+				bmpFactoryOptions.inJustDecodeBounds = false;
+				currentBitmap = BitmapFactory.decodeFile(tempPicPath, bmpFactoryOptions);
+				imageView.setImageBitmap(currentBitmap);
+				
+				// Delete the previous picture
+				if(null != currentFileName)
+					dao.deleteBitmap(currentFileName);
+				
+				currentFileName = ""+DateFormat.format("yyyyMMdd_hhmmss",
+						Calendar.getInstance(Locale.CHINA));
+				
+			}
+		}
+	}
+
+	@Override
+	public void onClick(View v) {
+		switch (v.getId()) {
+		case R.id.captureButton:
+			Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+			File tempPicture = new File(tempPicPath);
+			if(tempPicture.exists())
+				tempPicture.delete();
+			Uri uri = Uri.fromFile(tempPicture);
+			intent.putExtra(MediaStore.EXTRA_OUTPUT, uri);
+			intent.putExtra(MediaStore.EXTRA_VIDEO_QUALITY, 1);
+			startActivityForResult(intent, 1);
+			break;
+		case R.id.deleteButton:
+			currentBitmap = null;
+			imageView.setImageResource(R.drawable.ic_launcher);
+			break;
+		case R.id.resetButton:
+			resetActivity();
+			break;
+		case R.id.saveButton:
+			if(isReadyToSave()) {
+				
+				/** Do some real saving work here **/
+				dao.saveImageToSdcard(currentBitmap, currentFileName);
+				resetActivity();
+			} else {
+				Toast.makeText(this, productPrice.getText().toString(), 
+						Toast.LENGTH_SHORT).show();
+			}
+			/*Intent intent2 = new Intent();
+			intent2.setClass(this, MainActivity.class);
+			startActivity(intent2);
+			finish();*/
+			
+			break;
+
+		default:
+			break;
+		}
+	}
+	
+	
+	private void resetActivity() {
+		productName.setText("");
+		productDescription.setText("");
+		productPrice.setText("");
+		currentBitmap = null;
+		currentFileName = null;
+		imageView.setImageResource(R.drawable.ic_launcher);
+	}
+
+	private boolean isReadyToSave() {
+		if(null == currentBitmap || 
+				null == currentFileName ||
+				TextUtils.isEmpty(productName.getText().toString()) ||
+				TextUtils.isEmpty(productDescription.getText().toString()) ||
+				TextUtils.isEmpty(productPrice.getText().toString())) {
+			return false;
+		} else {
+			return true;
+		}
+	}
+	
 }
